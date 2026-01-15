a1617c37c236a11ce634f34aacbdee18962f6b21
viktig_sau <elmerbleiktvedt@hotmail.com>
2026-01-15 12:55:13 +0100

    made a first draft for a new calendar item endpoint

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template("index.html")
 
-@app.route("/api/celender/<calender_id>/new", methods=['POST'])
+@app.route("/api/celender/<calendar_id>/new", methods=['POST'])
 def new_calendar_item(calendar_id: str):
-    data = flask.request.json
+    try:
+        data = helpers.StrictDict(flask.request.json, type_map={
+            "title":str,
+            "description":(str, None),
+            "start":str,
+            "end":str,
+            "color":(str, None),
+        })
+    except TypeError:
+        return flask.jsonify({"status":"error", "error":"invalid data"}), 400
 
-
+    return flask.jsonify(data), 501
 
 if __name__ == "__main__":
     app.run(debug=True)
